e1171ec9ab652ebd6f3649501eac84f1a0842384
sbetancourtp <sbetancourtp@unal.edu.co>
2026-02-02 17:48:07 -0500

    Update flask course

--- estudo_flask/views.py
+++ estudo_flask/views.py
@@ -1,6 +1,10 @@
 from estudo_flask import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/nova/')
+def novapag():
+    return 'outra view'
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from estudo_flask import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
     print('Test')
